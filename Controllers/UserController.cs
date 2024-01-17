8be2c7bade9784514f2370a6425ba2c5b1dad483
Sahil Shrestha <35665722+sahilian@users.noreply.github.com>
2024-01-17 12:52:04 +0545

    admin controls added

---
+++ Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Swarojgaar.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index()
+        {
+            var roles =  _roleManager.Roles;
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+
+    }
+}
